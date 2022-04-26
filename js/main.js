e8a9e37baa26e0f5108e0a2e9af0b44476f1450c
brili24 <brili24@users.noreply.github.com>
2022-04-26 06:00:59 +0000

    deploy: 2f326f3bad75dfb828b9302e1504374eaab9c2cd

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
+var active = 0;
+var previousButton = document.querySelector("#previous");
+var nextButton = document.querySelector("#next");
+var slideshow = document.querySelectorAll(".slideshow > div");
 
+function setUp() {
+  slideshow.forEach((slide, i) => {
+    slide.dataset["index"] = i + 1;
+  });
+}
+function toggleSlide(from, to) {
+  slideshow[from].classList.remove("active");
+  slideshow[to].classList.add("active");
+}
+function previousSlide() {
+  var from = active;
+  if (active <= 0) {
+    active = slideshow.length - 1;
+  } else {
+    active--;
+  }
+  toggleSlide(from, active);
+}
+function nextSlide() {
+  var from = active;
+  if (active >= slideshow.length - 1) {
+    active = 0;
+  } else {
+    active++;
+  }
+  toggleSlide(from, active);
+}
+setUp();
+previousButton.addEventListener("click", previousSlide);
+nextButton.addEventListener("click", nextSlide);
